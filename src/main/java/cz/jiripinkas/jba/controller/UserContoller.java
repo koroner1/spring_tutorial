126c949f6a403a009989678fbef7bb94b10baab5
Mateusz Czub <mateusz.czub.1989@gmail.com>
2016-08-01 18:23:22 +0200

    tutorial 29, creating option - new blog

--- src/main/java/cz/jiripinkas/jba/controller/UserContoller.java
+++ src/main/java/cz/jiripinkas/jba/controller/UserContoller.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import cz.jiripinkas.jba.entity.Blog;
 import cz.jiripinkas.jba.entity.User;
+import cz.jiripinkas.jba.service.BlogService;
 import cz.jiripinkas.jba.service.UserService;
 
 @Controller
@@ -20,9 +22,17 @@ public class UserContoller {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private BlogService blogService;
+	
 	@ModelAttribute("user")
-	public User construct(){
+	public User constructUser(){
 		return new User();
+	}
+	
+	@ModelAttribute("blog")
+	public Blog constructBlog(){
+		return new Blog();
 	}
 	
 	@RequestMapping("/users")
@@ -54,6 +64,14 @@ public class UserContoller {
 		model.addAttribute("user", userService.findOneWithBlogs(name));
 		return "user-detail";
 	}
+	
+	@RequestMapping(value="/account", method=RequestMethod.POST)
+	public String doAddBlog(@ModelAttribute("blog") Blog blog, Principal principal){
+		String name = principal.getName();
+		blogService.save(blog, name);
+		return "redirect:/account.html";
+	}
+
 	
 	
 
