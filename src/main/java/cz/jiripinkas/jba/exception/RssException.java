57111b272de3da4c394e4cb1e19c6692a7b40e5a
Mateusz Czub <mateusz.czub.1989@gmail.com>
2016-08-04 17:44:20 +0200

    Tutorial 43, wyciaganie danych z pliku XML

---
+++ src/main/java/cz/jiripinkas/jba/exception/RssException.java
@@ -0,0 +1,11 @@
+package cz.jiripinkas.jba.exception;
+
+public class RssException extends Exception{
+
+	public RssException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
